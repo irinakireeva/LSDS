fc78f38329815db16e95f3548cb768ce2700589b
davidlegarre <77801298+davidlegarre@users.noreply.github.com>
2021-01-27 14:00:02 +0100

    FileLanguageFilter

---
+++ lab1/src/main/java/upf/edu/filter/FileLanguageFilter.java
@@ -0,0 +1,30 @@
+package upf.edu.filter;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class FileLanguageFilter {
+    private final String InputFile;
+    private final String OutputFile;
+
+    public FileLanguageFilter(String inputFile, String outputFile) {
+        InputFile = inputFile;
+        OutputFile = outputFile;
+    }
+
+    public void filterLanguage(String language) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(this.InputFile));
+        FileWriter writer = new FileWriter(this.OutputFile);
+        BufferedWriter bw = new BufferedWriter(writer);
+
+        String line = br.readLine();
+			while (line != null) {
+				System.out.println(line);
+				// read next line
+				line = br.readLine();
+
+        br.close();
+        bw.close();
+
+    }
+}
