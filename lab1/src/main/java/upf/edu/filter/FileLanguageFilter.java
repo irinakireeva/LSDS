41968afd7d2453bf2138887bb087864d3a061823
Vladimir Trukhaev <42299659+vladimirtrukhaev@users.noreply.github.com>
2021-01-27 13:17:40 +0100

    Add files via upload

---
+++ lab1/src/main/java/upf/edu/filter/FileLanguageFilter.java
@@ -0,0 +1,12 @@
+package upf.edu.filter;
+
+public class FileLanguageFilter implements LanguageFilter {
+    public FileLanguageFilter(String inputFile, String outputFile) {
+
+    }
+
+    @Override
+    public void filterLanguage(String language) throws Exception {
+
+    }
+}
