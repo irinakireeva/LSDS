0bd03f8d80247bdabdf53ce5db65c661264208ae
Vladimir Trukhaev <42299659+vladimirtrukhaev@users.noreply.github.com>
2021-02-03 19:40:00 +0100

    Update TwitterFilter.java

--- lab1/src/main/java/upf/edu/TwitterFilter.java
+++ lab1/src/main/java/upf/edu/TwitterFilter.java
@@ -1,7 +1,6 @@
 package upf.edu;
 
 import upf.edu.filter.FileLanguageFilter;
-import upf.edu.filter.FilterException;
 import upf.edu.uploader.S3Uploader;
 
 import java.io.IOException;
@@ -9,13 +8,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TwitterFilter {
-    public static void main( String[] args ) throws IOException {
+    public static void main(String[] args) throws IOException {
         List<String> argsList = Arrays.asList(args);
         String language = argsList.get(0);
         String outputFile = argsList.get(1);
         String bucket = argsList.get(2);
         System.out.println("Language: " + language + ". Output file: " + outputFile + ". Destination bucket: " + bucket);
-        for(String inputFile: argsList.subList(3, argsList.size())) {
+        for (String inputFile : argsList.subList(3, argsList.size())) {
             System.out.println("Processing: " + inputFile);
             final FileLanguageFilter filter = new FileLanguageFilter(inputFile, outputFile);
             filter.filterLanguage(language);
