ae2e3c41f6cbaca51179d4bad1b2dc3d390db01c
Vladimir Trukhaev <42299659+vladimirtrukhaev@users.noreply.github.com>
2021-02-03 19:39:17 +0100

    Update FileLanguageFilter.java

--- lab1/src/main/java/upf/edu/filter/FileLanguageFilter.java
+++ lab1/src/main/java/upf/edu/filter/FileLanguageFilter.java
@@ -2,23 +2,28 @@ package upf.edu.filter;
 
 import java.io.*;
 
-public class FileLanguageFilter implements LanguageFilter {
-    public FileLanguageFilter(String inputFile, String outputFile) throws IOException {
-        FileReader reader = new FileReader(inputFile);
-        BufferedReader bReader = new BufferedReader(reader);
+public class FileLanguageFilter {
+    private final String InputFile;
+    private final String OutputFile;
 
-        String line = bReader.readLine();
-
-        FileWriter writer = new FileWriter(outputFile);
-        BufferedWriter bWriter = new BufferedWriter(writer);
-
-        bWriter.write(line);
-
-        bReader.close();
-        bWriter.close();
+    public FileLanguageFilter(String inputFile, String outputFile) {
+        InputFile = inputFile;
+        OutputFile = outputFile;
     }
 
-    public void filterLanguage(String language) throws Exception {
+    public void filterLanguage(String language) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(this.InputFile));
+        FileWriter writer = new FileWriter(this.OutputFile);
+        BufferedWriter bw = new BufferedWriter(writer);
+
+        String line = br.readLine();
+        while (line != null) {
+            System.out.println(line);
+            // read next line
+            line = br.readLine();
+        }
+        br.close();
+        bw.close();
 
     }
 }
