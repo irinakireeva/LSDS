3a69d056cf6b469fe6865aad1b82e73917e61d8f
Vladimir Trukhaev <42299659+vladimirtrukhaev@users.noreply.github.com>
2021-02-03 19:40:36 +0100

    Update SimplifiedTweet.java

--- lab1/src/main/java/upf/edu/parser/SimplifiedTweet.java
+++ lab1/src/main/java/upf/edu/parser/SimplifiedTweet.java
@@ -4,8 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
-import java.io.Serializable;
 import java.util.Optional;
 
 public class SimplifiedTweet {
