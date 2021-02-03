99d77de8ff8ae4d40f972677a067d2e29221a9df
Vladimir Trukhaev <42299659+vladimirtrukhaev@users.noreply.github.com>
2021-02-03 19:38:07 +0100

    Add files via upload

---
+++ lab1/src/main/java/upf/edu/uploader/S3Uploader.java
@@ -0,0 +1,14 @@
+package upf.edu.uploader;
+
+import java.util.List;
+
+public class S3Uploader {
+
+
+    public S3Uploader(String bucket, String prefix, String upf) {
+    
+    }
+
+    public void upload(List<String> asList) {
+    }
+}
