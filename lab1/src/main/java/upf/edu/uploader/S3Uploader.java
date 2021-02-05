4dc71c001666087f84ae7dfac8ad7671e9b5ea01
Vladimir Trukhaev <42299659+vladimirtrukhaev@users.noreply.github.com>
2021-02-05 19:14:28 +0100

    Update S3Uploader.java

--- lab1/src/main/java/upf/edu/uploader/S3Uploader.java
+++ lab1/src/main/java/upf/edu/uploader/S3Uploader.java
@@ -3,10 +3,14 @@ package upf.edu.uploader;
 import java.util.List;
 
 public class S3Uploader {
-
+    private final String Bucket;
+    private final String Prefix;
+    private final String UPF;
 
     public S3Uploader(String bucket, String prefix, String upf) {
-
+        Bucket = bucket;
+        Prefix = prefix;
+        UPF = upf;
     }
 
     public void upload(List<String> asList) {
