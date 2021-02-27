a28c8072dfc02d78ec00c6e4a1af03ccbfaa625e
davidlegarre <david.legarre01@estudiant.upf.edu>
2021-02-27 23:21:36 +0100

    Por fin funciona ex 1

--- src/main/java/upf/edu/TwitterLanguageFilterApp/TwitterLanguageFilterApp.java
+++ src/main/java/upf/edu/TwitterLanguageFilterApp/TwitterLanguageFilterApp.java
@@ -1,8 +1,10 @@
 package upf.edu.TwitterLanguageFilterApp;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
+import upf.edu.TwitterLanguageFilterApp.parser.SimplifiedTweet;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -11,17 +13,17 @@ import java.util.List;
 public class TwitterLanguageFilterApp {
     public static void main(String[] args) throws IOException {
 
+        BasicConfigurator.configure();
         SparkConf conf = new SparkConf().setAppName("TwitterLanguageFilter");
         JavaSparkContext sc = new JavaSparkContext(conf);
-
+        System.out.println("Hola?");
         List<String> argsList = Arrays.asList(args);
         String language = argsList.get(0);
-        String outputFile = argsList.get(1);
-
-        System.out.println("Language: " + language + ". Output file: " + outputFile);
-        for (String inputFile : argsList.subList(2, argsList.size())) {
-            System.out.println("Processing: " + inputFile);
-
-        }
+        String inputFile = argsList.get(2);
+	    JavaRDD<String> lines = sc.textFile(inputFile);
+	    lines = lines.filter(tweet->tweet.length()>2);
+	    JavaRDD<SimplifiedTweet> tweets = lines.map(tweet->SimplifiedTweet.fromJson(tweet).get());
+	    tweets = tweets.filter(tweet->tweet.getLanguage().equals(language));
+	    tweets.saveAsTextFile(argsList.get(1));
     }
 }
